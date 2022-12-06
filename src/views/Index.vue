eeb2e4798182dd6c20f368d1ecf04e65c04f73d0
Natalya Prokhorova <105797271+natajedi@users.noreply.github.com>
2022-12-06 12:07:19 +0300

    Import json added to file

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,3 +1,14 @@
+<script>
+      import json from './server/db.json'
+      export default{
+          data(){
+              return{
+                  myJson: json
+              }
+          }
+      }
+</script>
+
 <template>
   <h1 class="heading-1">The Quiz</h1>
 </template>
