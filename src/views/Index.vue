daad269fc877591ffff0df1160e64c37987fc9a8
Alexey Uzlov <auzlov@codemastersintl.com>
2022-12-06 22:44:25 +0400

    * fixed some issues

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,14 +1,20 @@
 <script>
-      import json from './server/db.json'
-      export default{
-          data(){
-              return{
-                  myJson: json
-              }
-          }
+  export default {
+    mounted() {
+      fetch('http://localhost:3000/data').then(
+          (response) => response.json()
+      ).then((data) => this.myJson = data)
+    },
+    data() {
+      return {
+        myJson: null
       }
+    }
+  }
 </script>
 
 <template>
   <h1 class="heading-1">The Quiz</h1>
+
+  <pre>{{myJson}}</pre>
 </template>
