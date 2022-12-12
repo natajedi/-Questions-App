4cab0a427b8422f6b854789fcc57efe09c833a40
Natalya Prokhorova <105797271+natajedi@users.noreply.github.com>
2022-12-12 13:14:32 +0300

    Add files via upload

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,18 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: 'Button'
+  },
+  color: {
+    type: String,
+    default: 'primary'
+  }
+})
+</script>
+
 <template>
-    <div class="button">
-    </div>
+    <button :class="['btn', 'btn_${color}']">{{label}}</button>
 </template>
 
 <style lang="scss" scoped>
@@ -23,11 +35,23 @@
   }
 
   .btn {
-    margin-top: 15px;
-    align-self: flex-end;
-    background: none;
-    color: #180735;
-    border: 2px solid #13e7a8;
+    margin-right: 15px;
+    margin-left: 25px;
+    margin-bottom: 15px;
+    padding: 0 24px;
+    height: 40px;
+    color: #2e075a;
+    background-color: #67dbc2;
+    border-radius: 6px;
+    border: none;
+    cursor: pointer;
+    font-size: 16px;
+    transition: .2s;
+  }
+  .line {
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 20px;
   }
   
   @media (prefers-color-scheme: light) {
