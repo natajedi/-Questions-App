7ec701f79801667a98ccd377c5b659ab70cbb18f
Natalya Prokhorova <105797271+natajedi@users.noreply.github.com>
2023-01-12 08:30:15 +0300

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Index from '@/views/Index.vue'
 import CSS from '@/components/layout/Quiz_css.vue'
 import HTML from '@/components/layout/Quiz_html.vue'
 import JS from '@/components/layout/Quiz_js.vue'
+import Registration from '@/components/layout/Registration_form.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -27,6 +28,11 @@ const router = createRouter({
    name: 'JS',
    component: JS
   },
+  {
+    path: '/registration_form',
+    name: 'Registration',
+    component: Registration
+   }
 ]
 })
 
